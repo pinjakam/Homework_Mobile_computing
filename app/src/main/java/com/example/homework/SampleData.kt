42007c9a25139b1ae30a45fc37f3dc82e9a44a92
pinjakam <pkamula22@student.oulu.fi>
2026-01-22 20:17:53 +0200

    Homework 1 done.

---
+++ app/src/main/java/com/example/homework/SampleData.kt
@@ -0,0 +1,51 @@
+package com.example.homework
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Your friend",
+            "Good evening, I hope you had a great day!"
+        ),
+        Message(
+            "Your friend",
+            "If you did not have a great day, it's okay, tomorrow is a new day!"
+        ),
+        Message(
+            "Your friend",
+            "It is important to get enough sleep to feel well rested.\n" +
+                "Let me give you some tips on how to get a good night sleep!"
+        ),
+        Message(
+            "Your friend",
+            "Take a warm shower and wash all your thoughts away!"
+        ),
+        Message(
+            "Your friend",
+            "Dim the lights an hour before going to bed."
+        ),
+        Message(
+            "Your friend",
+            "Eat something light."
+        ),
+        Message(
+            "Your friend",
+            "Try putting your phone away and read a book instead."
+        ),
+        Message(
+            "Your friend",
+            "Try doing this short stretching routine before bed!.\n"+
+                   "Sit or stand comfortably and gently let your right ear drop toward your right shoulder.\n"+
+                    "Hold, breathe slowly, then switch sides.\n"+
+                    "This helps release tension from screens and daily posture.\n"+
+                    "Roll your shoulders backward in slow circles.\n"+
+                    "Interlace your fingers behind your back and gently lift your chest.\n"+
+                    "This counteracts slouching and opens the upper body.\n"+
+                    "Now you should feel more relaxed and ready for bed! :)"
+        ),
+        Message(
+            "Your friend",
+            "Good night, I hope you sleep well tonight!"
+        ),
+    )
+}
